da577682b380137e52837e3cfcaf688811d82a1b
Mario Medhat <87922718+Mario-Medhat@users.noreply.github.com>
2026-02-17 05:18:13 +0200

    Creating ExceptionHandlerMiddleware and injecting it

---
+++ NZWalks.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+
+namespace NZWalks.API.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly ILogger logger;
+        private readonly RequestDelegate next;
+
+        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger, RequestDelegate next)
+        {
+            this.logger = logger;
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                // Generate a unique error ID for tracking
+                var errorId = Guid.NewGuid();
+
+                // Log the exception details
+                logger.LogError(ex, $"{errorId} : Something went wrong: {ex.Message}");
+
+                // Return a generic error response to the client
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                httpContext.Response.ContentType = "application/json";
+
+                var errorResponse = new
+                {
+                    Id = errorId,
+                    message = "An unexpected error occurred. Please try again later."
+                };
+
+                await httpContext.Response.WriteAsJsonAsync(errorResponse);
+            }
+        }
+    }
+}
